7eff26da3d39b0bd9761156e33f2c38d0606a564
Patrik Švikruha <patrik.svikruha@gmail.com>
2022-04-26 13:07:36 +0200

    Add editorconfig sample

--- Net6/Test.cs
+++ Net6/Test.cs
@@ -6,32 +6,32 @@ using System.Threading.Tasks;
 
 namespace Net6
 {
-    //class Foo
-    //{
-    //    void M1()
-    //    {
-    //    } // RCS0010
-    //    void M2()
-    //    {
-    //    } // RCS0010
-    //    /// <summary>
-    //    /// ...
-    //    /// </summary>
-    //    void M3()
-    //    {
-    //    } // RCS0010
-    //    string P1 { get; set; } // RCS0010
-    //    [Obsolete]
-    //    string P2 { get; set; }
-    //} // RCS0010
-    //enum Bar
-    //{
-    //    A = 0, // RCS0010
-    //    /// <summary>
-    //    /// ...
-    //    /// </summary>
-    //    B = 1, // RCS0010
-    //    [Obsolete]
-    //    C = 2,
-    //}
+    class Foo
+    {
+       void M1()
+       {
+       } // RCS0010
+       void M2()
+       {
+       } // RCS0010
+       /// <summary>
+       /// ...
+       /// </summary>
+       void M3()
+       {
+       } // RCS0010
+       string P1 { get; set; } // RCS0010
+       [Obsolete]
+       string P2 { get; set; }
+    } // RCS0010
+    enum Bar
+    {
+       A = 0, // RCS0010
+       /// <summary>
+       /// ...
+       /// </summary>
+       B = 1, // RCS0010
+       [Obsolete]
+       C = 2,
+    }
 }
