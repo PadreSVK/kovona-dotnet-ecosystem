437305e60cfbbef8f391e1c42c5d1d8621cd4a39
Patrik Švikruha <patrik.svikruha@gmail.com>
2022-04-26 12:40:45 +0200

    Fix .NET6 project to VS2019 compatibility
    Enable .NET Core preview in VS2019 => https://docs.microsoft.com/en-us/answers/questions/620284/how-to-target-net-60-with-visual-studio-2019.html

--- Net6/Program.cs
+++ Net6/Program.cs
@@ -1,7 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
+using System;
+using System.Collections.Generic;
 using TestLibrary;
 
+namespace NetFramework
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            void Test(string name)
+            {
+                var tt = name ?? throw new ArgumentNullException(nameof(name), "blablabla bl bla");
 
-Console.WriteLine("Hello, World! net6");
-Console.WriteLine(Class1.Test);
+            }
+
+            Console.WriteLine("Hello, World! net6");
+            Console.WriteLine(Class1.Test);
+        }
+    }
+}
---
+++ Net6/Test.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Net6
+{
+    //class Foo
+    //{
+    //    void M1()
+    //    {
+    //    } // RCS0010
+    //    void M2()
+    //    {
+    //    } // RCS0010
+    //    /// <summary>
+    //    /// ...
+    //    /// </summary>
+    //    void M3()
+    //    {
+    //    } // RCS0010
+    //    string P1 { get; set; } // RCS0010
+    //    [Obsolete]
+    //    string P2 { get; set; }
+    //} // RCS0010
+    //enum Bar
+    //{
+    //    A = 0, // RCS0010
+    //    /// <summary>
+    //    /// ...
+    //    /// </summary>
+    //    B = 1, // RCS0010
+    //    [Obsolete]
+    //    C = 2,
+    //}
+}
